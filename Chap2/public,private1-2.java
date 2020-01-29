b2a2dafa6de18b806d10951a95bf90dce94e4152
leeejin <asd72621@naver.com>
2020-01-29 19:30:16 +0900

    java

---
+++ Chap2/public,private1-2.java
@@ -0,0 +1,10 @@
+package hiding;
+
+public class test {
+public static void main(String[] args)
+{
+	hiding studentlee = new hiding();
+	studentlee.set("이상원");
+	System.out.println(studentlee.get());
+}
+}
---
+++ Chap2/public,private1.java
@@ -0,0 +1,22 @@
+package hiding;
+
+public class hiding {
+
+	int id;
+	private String name;
+	int grade;
+	String address;
+	public String get()
+	{
+		return name;
+	}
+	public void set(String name)
+	{
+		this.name = name;
+	}
+	public static void main(String[] args)
+	{
+		
+		
+	}
+}
