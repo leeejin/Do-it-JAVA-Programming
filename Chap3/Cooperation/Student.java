d9e8a8d4c695a89fe41d820a25be16266443f674
leeejin <asd72621@naver.com>
2020-02-12 23:01:00 +0900

    객체 간 협력

---
+++ Chap3/Cooperation/Student.java
@@ -0,0 +1,28 @@
+package cooperation;
+
+public class Student {
+
+	public String name;
+	public int grade;
+	public int money;
+	public Student(String name , int money)
+	{
+		this.name = name;
+		this.money = money;
+	}
+		public void takeBus(Bus bus)
+		{
+			bus.take(1000);
+			this.money -= 1000;
+		}
+		public void takeSubway(Subway subway)
+		{
+			subway.take(1500);
+			this.money -= 1500;
+		}
+		public void showInfo()
+		{
+			System.out.println(name+"님의 남은 용돈은"+money);
+		}
+	
+}
