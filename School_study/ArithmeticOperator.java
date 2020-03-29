453c4374798b9ea66ad3d8434347abff661c9825
leeejin <asd72621@naver.com>
2020-03-29 23:25:09 +0900

    Scanner

---
+++ School_study/ArithmeticOperator.java
@@ -0,0 +1,19 @@
+package protect;
+import java.util.Scanner;
+public class ArithmeticOperator {
+public static void main(String[] args)
+{
+	Scanner scanner = new Scanner(System.in);
+	
+	System.out.print("정수를 입력하세요:");
+	int time = scanner.nextInt();
+	int second = time%60;
+	int minute = (time/60)%60;
+	int hour = (time/60)/60;
+	System.out.print(time+"초는");
+	System.out.print(hour+"시간,");
+	System.out.println(minute+"분,");
+	System.out.println(second+"초입니다. ");
+	scanner.close();
+}
+}
